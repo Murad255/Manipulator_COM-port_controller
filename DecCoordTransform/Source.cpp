1fb71e5e13ef8ef88cc1b375aea517e3450bbdbd
Murad <murad.abdulzagirov@gmail.com>
2019-03-23 00:00:13 +0300

    #BETTA-5.3 реализован алгоритм для поиска верхней точки  пересечения окружностей.

---
+++ DecCoordTransform/Source.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <math.h>
+#include "conio.h"
+#define PI 3.14
+#define L1	105
+#define L2	147
+
+using namespace std;
+
+class Coord
+{
+	int decX, decY, decZ, spR;
+	float spQ, spF;
+	float servQ, servA, servB;
+public:
+	Coord(int decX, int decY, int decZ);
+	void write(int decX, int decY, int decZ);
+	float givA() { return servA; }
+	float givB() { return servB; }
+	float givQ() { return servQ; }
+};
+
+
+int main() {
+	setlocale(0, "");
+	int x, y, z;
+	cout << "¬ведите координаты. \nX=";
+	cin >> x;
+	cout << "\nY = ";
+	cin >> y;
+	cout << "\nZ = ";
+	cin >> z;
+
+	Coord dec(x, y, z);
+	cout << '\n' << "угол A=" << dec.givA() << '\n' << "угол B=" << dec.givB() << '\n' << "угол Q=" << dec.givQ();
+
+	_getch();
+	return 0;
+}
+void Coord::write(int decX, int decY, int decZ) {
+	this->decX = decX;
+	this->decY = decY;
+	this->decZ = decZ;
+
+	spR = (int)sqrt(decX*decX + decY * decY + decZ * decZ);
+	spQ = (float)((atan(sqrt(decX*decX + decY * decY) / decZ)) * 180.0 / PI);
+	spF = (float)(atan(decY / decX) *180.0 / PI);
+
+	servQ = spQ;
+	servB = (float)(180 - (acos((L1*L1 + L2 * L2 - spR * spR) / (2 * L1*L2))) * 180.0 / PI);
+	servA = (float)(spF - (acos((-L1 * L1 + L2 * L2 - spR * spR) / (2 * L1*spR))) * 180 / PI);
+
+}
+
+Coord::Coord(int decX = 0, int decY = 0, int decZ = 0)
+{
+	write(decX, decY, decZ);
+}
---
+++ координата_пересечения_шаров/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+    class Dec
+    {
+        public double decX, decY, decZ, pXY;
+        public Dec(double decX = 0.0, double decY = 0.0, double decZ = 0.0)
+        {
+            this.decX = decX;
+            this.decY = decY;
+            this.decZ = decZ;
+            this.pXY = Math.Sqrt(decX * decX + decY * decY);
+        }
+    }
+
+    static class shape
+    {
+        const double L2 = 147;
+        const double L1 = 105;
+
+        static public Dec write(Dec decIn)
+        {
+            {
+                double val = Math.Sqrt(decIn.decX * decIn.decX + decIn.decY * decIn.decY + decIn.decZ * decIn.decZ);
+                if (((L1 + L2) < val) || ((L1 - L2) > val))
+                {
+                    throw new Exception("Координата выходит за пределы зоны обслуживания");
+                }
+            }
+            double X = decIn.pXY;
+            double Y = decIn.decZ;
+            double a = -2 * X;
+            double b = -2 * Y;
+            double c = X * X + Y * Y + L1 * L1 - L2 * L2;
+
+
+
+            double x0 = -a * c / (a * a + b * b), y0 = -b * c / (a * a + b * b);
+
+            double d = L1 * L1 - c * c / (a * a + b * b);
+            double mult = Math.Sqrt(d / (a * a + b * b));
+            double ax, ay, bx, by;
+            ax = x0 + b * mult;
+            bx = x0 - b * mult;
+            ay = y0 - a * mult;
+            by = y0 + a * mult;
+            Dec decOut = new Dec();
+            if (ay > by)
+            {
+                decOut.pXY = ax;
+                decOut.decZ = ay;
+            }
+            else
+            {
+                decOut.pXY = bx;
+                decOut.decZ = by;
+            }
+            if (decIn.decX == 0) decOut.decX = 0;
+            else decOut.decX = decIn.pXY / decIn.decX * decOut.pXY;
+            if (decIn.decY == 0) decOut.decY = 0;
+            else decOut.decY = decIn.pXY / decIn.decY * decOut.pXY;
+
+            return decOut;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите значения координат.\nX= ");
+            //int x=Console.Read();
+            // Console.WriteLine("\nY=");
+            // int y = Console.Read();
+            // Console.WriteLine("\nZ=");
+            // int z = Console.Read();
+            int x = 100, y = 50, z = 50;
+            Dec dec = new Dec(x, y, z);
+            Dec outDec = shape.write(dec);
+            Console.WriteLine("Координаты верхней точки  пересечения шаров:");
+            Console.WriteLine("\nX=");
+            Console.Write(outDec.decX);
+            Console.WriteLine("\nY=");
+            Console.Write(outDec.decY);
+            Console.WriteLine("\nZ=");
+            Console.Write(outDec.decZ);
+
+            Console.Read();
+        }
+    }
+}
